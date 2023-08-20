36ef3c93f1cdd8025b0fa7af84558e9d277603b2
Russell Bunch <doomslayer@hpe.com>
2023-08-20 14:52:24 -0500

    Update `os` module
    This updates the `os` module to no longer assume an encoding, making it
    more versatile.

--- crucible/tests/test_os.py
+++ crucible/tests/test_os.py
@@ -22,9 +22,13 @@
 #  OTHER DEALINGS IN THE SOFTWARE.
 #
 """
-Tests for the ``cmd`` module.
+Tests for the ``os`` module.
 """
 from os import getcwd
+from subprocess import Popen
+
+import pytest
+import mock
 
 from crucible.os import run_command
 from crucible.os import chdir
@@ -32,8 +36,10 @@ from crucible.os import chdir
 
 class TestCLI:
     """
-    Test class for the ``cli`` module.
+    Test class for the ``os`` module.
     """
+
+    init = 0.0
 
     def test_run_command(self) -> None:
         """
@@ -49,14 +55,58 @@ class TestCLI:
         command_result = run_command(command)
         for result in [no_shell_result, shell_result, bad_result,
                        command_result]:
-            assert result.duration > 0.0
+            assert result.duration > self.init
             assert isinstance(result.return_code, int)
             assert result.stdout or result.stderr
+
+    def test_run_command_decode(self) -> None:
+        """
+        Assert that a ``_CLI`` object successfully decodes its ``stdout`` and
+        ``stderr``.
+        """
+        command = ['ls', '-l']
+        shell_command = 'ls -l'
+        bad_command = 'foo!!!'
+        no_shell_result = run_command(shell_command)
+        shell_result = run_command(
+            shell_command,
+            in_shell=True,
+            charset='utf-8'
+        )
+        bad_result = run_command(bad_command)
+        command_result = run_command(command)
+        for result in [no_shell_result, shell_result, bad_result,
+                       command_result]:
+            assert result.duration > self.init
+            assert isinstance(result.return_code, int)
+            assert result.stdout or result.stderr
+
+    def test_run_command_decode_unicode_error(self) -> None:
+        """
+        Assert that ``UnicodeDecodeErrors`` are caught by the ``run_command``
+        and a non-zero return code is received.
+        """
+        # Test using a CP1252 (windows ostensibly) character "œ"
+        byte_string = b'\x9c'
+        with mock.patch.object(
+                Popen,
+                'communicate',
+                return_value=(byte_string, byte_string)
+        ):
+            result = run_command(['ls', '-l'])
+            assert result.duration > self.init
+            assert isinstance(result.return_code, int)
+            assert result.stdout or result.stderr
+            assert isinstance(result.stdout, bytes)
+            assert isinstance(result.stderr, bytes)
+            with pytest.raises(UnicodeDecodeError):
+                run_command(['ls', '-l'], charset='utf-8')
 
     def test_chdir(self) -> None:
         """
         Assert that our context manager will change directories and
         change back to our original directory.
+
         """
         original = getcwd()
         with chdir('/'):
